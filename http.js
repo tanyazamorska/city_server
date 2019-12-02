2835180fb1de9b486084df9d6d669b6386945bad
Taras Ivashchuk <tarasivashchuk@gmail.com>
2019-12-02 22:49:49 +0200

    deleted

--- http.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var server = http.createServer();
-server.on('request', function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-})
-server.listen(3100);
-console.log('Server running at http://localhost:3100/');
